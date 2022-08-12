ea5edd47814d4764b7fd8a82c6c0b8a934e2400b
CallumRosam <104946983+CallumRosam@users.noreply.github.com>
2022-08-12 18:12:36 +0100

    1st update

--- exercises/B-boolean-literals/exercise.js
+++ exercises/B-boolean-literals/exercise.js
@@ -6,7 +6,11 @@
 */
 
 let codeYourFutureIsGreat = true;
+let mozafarIsCool = false;
+let calculationCorrect = true;
 
+
+// console.log("Is Code Your Future great?", codeYourFutureIsGreat);
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
@@ -14,7 +18,7 @@ let codeYourFutureIsGreat = true;
 console.log("Is Code Your Future great?", codeYourFutureIsGreat);
 console.log("Is Mozafar cool?", mozafarIsCool);
 console.log("Does 1 + 1 = 2?", calculationCorrect);
-console.log("Are there more than 10 students?", moreThan10Students);
+ 
 
 /* 
   EXPECTED RESULT
